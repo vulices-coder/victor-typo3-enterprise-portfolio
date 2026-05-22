afdef13c80e9aea8dcc2d75d00a58f6a472ae1ae
Victor Flores <v.ulices@gmail.com>
2026-05-22 14:21:11 +0200

    chore: register victor sitepackage as TYPO3 extension

---
+++ typo3/packages/victor_sitepackage/ext_emconf.php
@@ -0,0 +1,16 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Victor Sitepackage',
+    'description' => 'Enterprise TYPO3 sitepackage for Victor Flores portfolio.',
+    'category' => 'templates',
+    'author' => 'Victor Flores',
+    'author_email' => 'v.ulices@gmail.com',
+    'state' => 'alpha',
+    'version' => '0.1.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '13.4.0-13.4.99',
+        ],
+    ],
+];
